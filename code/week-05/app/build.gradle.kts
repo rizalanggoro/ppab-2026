b89e2fffd2efb196533f4d43b4cf00e46181e715
Rizal Anggoro <gnoogler4@gmail.com>
2026-04-15 09:27:30 +0700

    feat: implement login toggle functionality in AuthScreen and update Gradle dependencies

--- code/week-05/app/build.gradle.kts
+++ code/week-05/app/build.gradle.kts
@@ -50,8 +50,10 @@ dependencies {
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
+    // material 3 alpha
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.material.icons.extended)
+    // end material 3 alpha
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- code/week-05/app/src/main/java/com/asprak/todolistd/feature/auth/presentation/AuthScreen.kt
+++ code/week-05/app/src/main/java/com/asprak/todolistd/feature/auth/presentation/AuthScreen.kt
@@ -12,6 +12,10 @@ import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -24,7 +28,13 @@ import com.asprak.todolistd.ui.theme.TodoListTheme
 fun AuthScreen() {
     val backStack = LocalBackStack.current
 
+    var isLogin by remember { mutableStateOf(true) }
+
     Content(
+        isLogin = isLogin,
+        onClickSwitch = {
+            isLogin = !isLogin
+        },
         onClickSubmit = {
             backStack.add(Routes.ListTodoRoute)
 
